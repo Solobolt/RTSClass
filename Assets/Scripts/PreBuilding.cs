26667a460d1851e744f06a311fce2fd6865d405e
Solobolt <solobolt@gmail.com>
2015-11-29 12:30:23 +1000

    IMplimenetd selection boxes and stoped building from being built on to of eachother

--- Assets/Scripts/PreBuilding.cs
+++ Assets/Scripts/PreBuilding.cs
@@ -3,15 +3,21 @@ using System.Collections;
 
 public class PreBuilding : MonoBehaviour {
     Transform myTransform;
+    Renderer myRenderer;
 
 	SelectionMovement selection;
 
 	private bool placed = false;
+    private bool placeable = true;
     public GameObject towerToPlace;
+
+    public Material canBuildShader;
+    public Material noBuildShader;
 
 	// Use this for initialization
 	void Start () {
         myTransform = this.transform;
+        myRenderer = this.GetComponent<Renderer>();
 		selection = GameObject.FindGameObjectWithTag ("GameController").GetComponent<SelectionMovement>();
 	}
 	
@@ -21,16 +27,58 @@ public class PreBuilding : MonoBehaviour {
 		//PreBuilding Follow Curser
 		if(selection.placingBuilding == true && placed == false)
 		{
-			transform.position = selection.mousePosition;
+            Vector3 towerPos = selection.mousePosition;
+            towerPos.y = 0.5f * myTransform.localScale.y;
+            myTransform.position = towerPos;
 		}
 
 		//prebuilding placements
 		if(Input.GetMouseButton (0))
 		{
-			selection.placingBuilding = false;
-            placed = true;
-            Instantiate(towerToPlace,myTransform.position,myTransform.rotation);
-            Destroy(this.gameObject);
+            if (placeable == true)
+            {
+                selection.placingBuilding = false;
+                placed = true;
+                Instantiate(towerToPlace, myTransform.position, myTransform.rotation);
+                Destroy(this.gameObject);
+            }
 		}
+
+        ChangeRenderer();
 	}
+
+    //checks if the building collides with an object
+    void OnTriggerEnter(Collider otherObject)
+    {
+        if(otherObject.transform.tag == "Tower")
+        {
+            placeable = false;
+        }
+    }
+
+    void OnTriggerStay(Collider otherObject)
+    {
+        if (otherObject.transform.tag == "Tower")
+        {
+            placeable = false;
+        }
+    }
+
+    void OnTriggerExit(Collider otherObject)
+    {
+        placeable = true;
+    }
+
+    //Changes the materail depending on weather or not the player can build
+    void ChangeRenderer()
+    {
+        if(placeable == false)
+        {
+            myRenderer.material = noBuildShader;
+        }
+        else
+        {
+            myRenderer.material = canBuildShader;
+        }
+    }
 }
--- Assets/Scripts/SelectionMovement.cs
+++ Assets/Scripts/SelectionMovement.cs
@@ -9,11 +9,15 @@ public class SelectionMovement : MonoBehaviour {
 	public bool placingBuilding = false;
 	public GameObject preBuilding;
     public GameObject finalObject;
+    private GameObject currentBox;
 
     //multiUnitSelection
     private Vector3 clickPos;
     public GameObject selectionBox;
     private bool hasSelected = true;
+    private bool boxCreated = false;
+    private Ray _Ray;
+    private RaycastHit _Hit;
 
 	//mouse clicking 
 	public Vector3 mousePosition;
@@ -26,18 +30,18 @@ public class SelectionMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		locateMouse ();
+        CreateTower();
 	}
 
 	//locates the mouse pointer in the world
 	void locateMouse()
 	{
 		//Fire ray at world
-		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-		RaycastHit hit;
+		_Ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 		
-		if(Physics.Raycast (ray,out hit))
+		if(Physics.Raycast (_Ray,out _Hit))
 		{
-			mousePosition = hit.point;
+			mousePosition = _Hit.point;
 		}
 
 
@@ -51,32 +55,31 @@ public class SelectionMovement : MonoBehaviour {
         if (Input.GetMouseButton (0))
 		{
 			//if targeted object is tagged as player, make it selected object
-			if(hit.transform.tag=="Player" && placingBuilding == false)
+			if(_Hit.transform.tag=="Player" && placingBuilding == false)
 			{
-				selectedUnit = hit.transform.gameObject;
+				selectedUnit = _Hit.transform.gameObject;
 			}
 
-			if(hit.transform.tag != "Player")
+			if(_Hit.transform.tag != "Player")
 			{
 				selectedUnit = null;
 			}
 
+            //Drags a box collider to sellect multiple units at once
             if (mousePosition != clickPos)
             {
-                print("Mouse Has moved");
-                if(!hasSelected)
-                {
-                    GameObject _SelectionBox = Instantiate(selectionBox, mousePosition, new Quaternion(0, 0, 0, 0)) as GameObject;
-                    hasSelected = true;
-                }
-                
+                CreateSelectionBox();
             }
 		}
 
+       
+
+        //decdes what happens when the player lifts thier mouse buttopn
         if(Input.GetMouseButtonUp(0))
         {
-            
-            hasSelected = false;
+            Destroy(currentBox);
+            boxCreated = false;
+            hasSelected = true;
         }
 
 		//left click, move selected unit to position
@@ -87,20 +90,56 @@ public class SelectionMovement : MonoBehaviour {
 
 		}
 
-		//placing pre building
-		if(Input.GetKeyDown("f") && placingBuilding == false)
-		{
-			placingBuilding = true;
-			selectedUnit = null;
-			GameObject PreTower = Instantiate (preBuilding,mousePosition,hit.transform.rotation) as GameObject;
 
-            if(finalObject != null)
+		
+	}
+
+    //Creates a pre tower and then places down a tower when clicked
+    void CreateTower()
+    {
+        //placing pre building
+        if (Input.GetKeyDown("f") && placingBuilding == false)
+        {
+            placingBuilding = true;
+            selectedUnit = null;
+            GameObject PreTower = Instantiate(preBuilding, mousePosition, _Hit.transform.rotation) as GameObject;
+
+            if (finalObject != null)
             {
                 PreTower.GetComponent<Transform>().localScale = finalObject.GetComponent<Transform>().localScale;
                 PreTower.GetComponent<PreBuilding>().towerToPlace = finalObject;
             }
-		}
-	}
+        }
+    }
 
+    //Creates a selection box and changes its transform and scale to accompany mouse movement
+    //NOTE: Selection box must be trigger
+    void CreateSelectionBox()
+        {
+        hasSelected = false;
+        if (hasSelected == false)
+        {
+            if (boxCreated == false)
+            {
+                GameObject _SelectionBox = Instantiate(selectionBox, mousePosition, new Quaternion(0, 0, 0, 0)) as GameObject;
+                currentBox = _SelectionBox;
+                boxCreated = true;
+            }
+
+            //scales the box collider depending on how the mouse moved
+            float xScale = clickPos.x - mousePosition.x;
+            float yScale = clickPos.z - mousePosition.z;
+
+            //moves the box collider to that one corner is at the point of the mouse click and the other is where the mouse currently is                    
+            float xPos = (xScale / 2.0f) - clickPos.x;
+            float yPos = 2.5f;
+            float zPos = (yScale / 2.0f) - clickPos.z;
+
+            //sets the scale and position of thje box collider
+            currentBox.GetComponent<Transform>().localScale = new Vector3(xScale, 5, yScale);
+            currentBox.GetComponent<Transform>().position = new Vector3(-xPos, yPos, -zPos);
+            hasSelected = false;
+        }
+    }
 
 }
