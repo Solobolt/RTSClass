d3cb720a23a4b27079d350d9037eda4e67c8408f
Solobolt <solobolt@gmail.com>
2015-12-06 22:43:47 +1000

    Created wall tower and edited nav mesh setttings

---
+++ Assets/Scripts/Towers/Wall.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Wall : Tower {
+
+    public override void StartTowerEffect()
+    {
+        //Nothing
+    }
+
+    public override void UpdateTowerEffect()
+    {
+        //nothing
+    }
+}
